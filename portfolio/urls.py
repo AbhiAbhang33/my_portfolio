88dbc8b480e86900f51a25e620910fcc2fcabdb5
Abhi <abhangabhaishek99200@gmail.com>
2024-11-20 09:13:14 +0530

    Final Commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from django_distill import distill_path
 from .views import home
 
-# This function tells django-distill the parameters required for the `home` view
-def get_index():
-    return None  # `None` because no parameters are required
-
 urlpatterns = [
-    distill_path('', home, name='home', distill_func=get_index),
+    path('', home, name='home'),
 ]
